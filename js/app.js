f6645a820f61870237dc4335f020ee90bd9c02f6
adiyy2001 <adiy@onet.pl>
2019-09-23 18:28:37 +0200

    My first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+document.getElementsByClassName("btn-1")[0].addEventListener("click", function(){ 
+    document.getElementsByClassName('art-3')[0].style.left = "180px";
+    document.getElementsByClassName('art-3')[0].style.opacity = 1;
+
+    document.getElementsByClassName('position-2')[0].style.left = "180px";
+    document.getElementsByClassName('position-2')[0].style.opacity = 1;
+
+    document.getElementsByClassName('position-1')[0].style.right = "180px";
+    document.getElementsByClassName('position-1')[0].style.opacity = 1;
+
+    document.getElementsByClassName('art-4')[0].style.right = "180px";
+    document.getElementsByClassName('art-4')[0].style.opacity = 1;
+});
